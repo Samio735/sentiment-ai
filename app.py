cd1ab235badbe4343f15b6e3a6c3ef4c66905dd4
Samy Rahim <102865808+Samio735@users.noreply.github.com>
2023-09-23 05:54:28 +0000

    finish v0

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import re
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
+
 app = Flask(__name__)
 
 
@@ -14,30 +15,39 @@ with open('trained_model.pkl', 'rb') as model_file:
     # Load the saved tokenizer (you should have saved it during training)
     with open('tokenizer.pkl', 'rb') as tokenizer_file:
         tokenizer = pickle.load(tokenizer_file)
+    def predict_sentiment(input_phrase):
+        input_phrase = input_phrase.lower()
+        input_phrase = re.sub('[^a-zA-z0-9\s]', '', input_phrase)
+        MAX_SEQUENCE_LENGTH = 393  # Replace with the same value you used for training
+        # Tokenize and pad the input sequence
+        input_seq = tokenizer.texts_to_sequences([input_phrase])
+        input_seq = pad_sequences(input_seq, maxlen=MAX_SEQUENCE_LENGTH)  # Use the same MAX_SEQUENCE_LENGTH as during training
 
-    # Preprocess the input phrase
-    input_phrase = "neutral"  # Replace with your actual input
-    input_phrase = input_phrase.lower()
-    input_phrase = re.sub('[^a-zA-z0-9\s]', '', input_phrase)
-    MAX_SEQUENCE_LENGTH = 393  # Replace with the same value you used for training
-    # Tokenize and pad the input sequence
-    input_seq = tokenizer.texts_to_sequences([input_phrase])
-    input_seq = pad_sequences(input_seq, maxlen=MAX_SEQUENCE_LENGTH)  # Use the same MAX_SEQUENCE_LENGTH as during training
+        # Make predictions
+        predicted_sentiment = model.predict(input_seq)
 
-    # Make predictions
-    predicted_sentiment = model.predict(input_seq)
+        # Convert predictions to sentiment labels
+        sentiment_labels = ['negative', 'positive']
+        predicted_label = sentiment_labels[np.argmax(predicted_sentiment)]
 
-    # Convert predictions to sentiment labels
-    sentiment_labels = ['negative', 'positive']
-    predicted_label = sentiment_labels[np.argmax(predicted_sentiment)]
-
-    print("Predicted Sentiment:", predicted_label)
+        return predicted_label
 
 
-@app.route('/')
-def home():
-    return 'Hello World!'
+    @app.route('/')
+    def home():
+        return 'Hello World!'
+    
+    @app.route('/predict', methods=['POST'])
+    def predict():
+        if request.method == 'POST':
+            data = request.get_json()
+            input_phrase = data['input_phrase']
+            predicted_label = predict_sentiment(input_phrase)
+            return jsonify({'predicted_label': predicted_label})
+            
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True)
+
+
